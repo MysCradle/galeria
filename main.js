e4fd30c8f39e2a3bde47e7ce07feacb2da194e88
Francisco Kaique <fkaiquesp@gmail.com>
2026-01-15 14:24:17 -0300

    feat: links no navbar

--- main.js
+++ main.js
@@ -129,3 +129,16 @@ criarPersonagem(Magico)
 criarPersonagem(Kama)
 
 mostrarPersonagens()
+
+
+const light = document.getElementById("light")
+
+light.addEventListener('click', ()=>{
+    if (document.body.classList.contains('tema--escuro')){
+        document.body.classList.remove('tema--escuro')
+        document.body.classList.add('tema--claro')
+    }else{
+        document.body.classList.remove('tema--claro')
+        document.body.classList.add('tema--escuro')
+    }
+})  
